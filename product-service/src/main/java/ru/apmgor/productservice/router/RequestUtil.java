f0687fb65b3950584e4d469d2cd40e26e22d1c31
Artur <apmgor@gmail.com>
2023-10-10 21:03:41 +0300

    Add Product Handler class

---
+++ product-service/src/main/java/ru/apmgor/productservice/router/RequestUtil.java
@@ -0,0 +1,10 @@
+package ru.apmgor.productservice.router;
+
+
+public class RequestUtil {
+    private RequestUtil() {}
+
+    public static final String ID = "id";
+    public static final String MAX = "max";
+    public static final String MIN = "min";
+}
