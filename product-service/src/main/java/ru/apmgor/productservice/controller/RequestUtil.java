a402f677f93856d431036ba1dda789e7dd67e3be
Artur <apmgor@gmail.com>
2023-10-11 19:41:48 +0300

    Minor fixes

--- product-service/src/main/java/ru/apmgor/productservice/controller/RequestUtil.java
+++ product-service/src/main/java/ru/apmgor/productservice/controller/RequestUtil.java
@@ -1,11 +1,8 @@
 package ru.apmgor.productservice.controller;
 
-
 public class RequestUtil {
     private RequestUtil() {}
 
     public static final String PRODUCT_PATH = "/products";
     public static final String ID = "id";
-    public static final String MAX = "max";
-    public static final String MIN = "min";
 }
