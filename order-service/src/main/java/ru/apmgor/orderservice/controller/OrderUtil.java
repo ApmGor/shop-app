5c342b6ca0e0df41637a1b462c497fcb91a0ba7c
Artur <apmgor@gmail.com>
2023-10-14 00:02:04 +0300

    Create OrderService and createOrder method

---
+++ order-service/src/main/java/ru/apmgor/orderservice/controller/OrderUtil.java
@@ -0,0 +1,7 @@
+package ru.apmgor.orderservice.controller;
+
+public class OrderUtil {
+    private OrderUtil() {}
+
+    public static final String ORDER_PATH = "/orders";
+}
---
+++ order-service/src/main/java/ru/apmgor/orderservice/service/OrderService.java
@@ -0,0 +1,55 @@
+package ru.apmgor.orderservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import ru.apmgor.orderservice.client.ProductServiceClient;
+import ru.apmgor.orderservice.client.UserServiceClient;
+import ru.apmgor.orderservice.dto.OrderRequestDto;
+import ru.apmgor.orderservice.dto.OrderResponseDto;
+import ru.apmgor.orderservice.dto.OrderStatus;
+import ru.apmgor.productservice.dto.ProductDto;
+import ru.apmgor.userservice.dto.TransactionStatus;
+import ru.apmgor.userservice.dto.UserTransactionDto;
+import ru.apmgor.userservice.dto.UserTransactionStatusDto;
+
+@Service
+@RequiredArgsConstructor
+public final class OrderService {
+
+    private final ProductServiceClient productClient;
+    private final UserServiceClient serviceClient;
+
+    public Mono<OrderResponseDto> createOrder(final Mono<OrderRequestDto> shareMono) {
+        return shareMono
+                .map(OrderRequestDto::getProductId)
+                .flatMap(productClient::getProductById)
+                .zipWith(shareMono)
+                .map(this::createUTSDtoFrom)
+                .flatMap(serviceClient::createUserTransaction)
+                .zipWith(shareMono)
+                .map(this::createORespDtoFrom);
+    }
+
+    private OrderResponseDto createORespDtoFrom(final Tuple2<UserTransactionStatusDto, OrderRequestDto> tuple) {
+        return new OrderResponseDto(
+                tuple.getT2().getUserId(),
+                tuple.getT2().getProductId(),
+                tuple.getT1().getAmount(),
+                getOrderStatusBy(tuple.getT1().getStatus())
+        );
+    }
+
+    private UserTransactionDto createUTSDtoFrom(final Tuple2<ProductDto, OrderRequestDto> tuple) {
+        return new UserTransactionDto(tuple.getT2().getUserId(), tuple.getT1().price());
+    }
+
+    private OrderStatus getOrderStatusBy(final TransactionStatus status) {
+        return switch (status) {
+            case APPROVED -> OrderStatus.COMPLETED;
+            case DECLINED -> OrderStatus.FAILED;
+        };
+    }
+}
